cdd606f192b54b0119009cfc882567c9d632ae07
Lapey <bisisolesi@gmail.com>
2023-11-26 14:23:45 +0000

    Added Forecast HTML and CSS

--- src/index.js
+++ src/index.js
@@ -52,7 +52,6 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
